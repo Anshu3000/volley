7eaff2f75120b98d5c09db91b557447e15be6dff
Anshulight <mrray7838035317@gmail.com>
2020-06-06 22:37:22 +0530

    json object Api

--- app/src/main/java/com/example/volley1/MainActivity.java
+++ app/src/main/java/com/example/volley1/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.volley1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.textclassifier.TextLinks;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+   private String url1 ="https://jsonplaceholder.typicode.com/posts/1";
+   private RequestQueue req;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         req= Volley.newRequestQueue(this);
+
+        JsonObjectRequest  jso =new JsonObjectRequest(Request.Method.GET, url1, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("json", "onResponse: "+response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("json", "onErrorResponse: "+"error");
+            }
+        });
+
+
+        req.add(jso);
     }
 }
